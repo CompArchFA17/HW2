2ab1870a1d2c070a90019f6e668cefc7be74759d
Jonah Spear <Jonahspear512@gmail.com>
2017-09-17 11:03:11 -0400

    make adder tests

--- adder.t.v
+++ adder.t.v
@@ -9,6 +9,23 @@ module testFullAdder();
     behavioralFullAdder adder (sum, carryout, a, b, carryin);
 
     initial begin
-        // Your test code here
-    end
+      // test when all wires are 0
+      $display("A B Cin | Cout Sum ");
+      a=0;b=0;carryin=0; #1000
+      $display("%b %b  %b  |  %b   %b", a, b, carryin, carryout, sum);
+      a=1;b=0;carryin=0; #1000
+      $display("%b %b  %b  |  %b   %b", a, b, carryin, carryout, sum);
+      a=0;b=1;carryin=0; #1000
+      $display("%b %b  %b  |  %b   %b", a, b, carryin, carryout, sum);
+      a=1;b=1;carryin=0; #1000
+      $display("%b %b  %b  |  %b   %b", a, b, carryin, carryout, sum);
+      a=0;b=0;carryin=1; #1000
+      $display("%b %b  %b  |  %b   %b", a, b, carryin, carryout, sum);
+      a=1;b=0;carryin=1; #1000
+      $display("%b %b  %b  |  %b   %b", a, b, carryin, carryout, sum);
+      a=0;b=1;carryin=1; #1000
+      $display("%b %b  %b  |  %b   %b", a, b, carryin, carryout, sum);
+      a=1;b=1;carryin=1; #1000
+      $display("%b %b  %b  |  %b   %b", a, b, carryin, carryout, sum);
+      end
 endmodule
