4c953358205070750bffb6945c48bd85e2b6f928
arianaolson419 <ariana.olson@students.olin.edu>
2017-09-17 23:07:02 -0400

    Successfully implement and test a structural decoder.

--- decoder.v
+++ decoder.v
@@ -1,5 +1,8 @@
 // Decoder circuit
 
+`define AND and #50
+`define NOT not #50
+
 module behavioralDecoder
 (
     output out0, out1, out2, out3,
@@ -17,6 +20,26 @@ module structuralDecoder
     input address0, address1,
     input enable
 );
-    // Your decoder code here
+    
+    wire nA0;
+    wire nA1;
+    wire nA0andnA1;
+    wire A0andnA1;
+    wire nA0andA1;
+    wire A0andA1;
+
+    `NOT A0inv(nA0, address0);
+    `NOT A1inv(nA1, address1);
+
+    `AND andgate00(nA0andnA1, nA0, nA1);
+    `AND andgate01(A0andnA1, address0, nA1);
+    `AND andgate02(nA0andA1, nA0, address1);
+    `AND andgate03(A0andA1, address0, address1);
+
+    `AND andgateout0(out0, enable, nA0andnA1);
+    `AND andgateout1(out1, enable, A0andnA1);
+    `AND andgateout2(out2, enable, nA0andA1);
+    `AND andgateout3(out3, enable, A0andA1);
+
 endmodule
 
