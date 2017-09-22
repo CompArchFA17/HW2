c1bd0365226453292270acbd3f6e59a342bf70af
kuannie1 <kuannie1@gmail.com>
2017-09-22 15:44:50 -0400

    Updating my HW2 repo

--- adder.t.v
+++ adder.t.v
@@ -6,9 +6,31 @@ module testFullAdder();
     reg a, b, carryin;
     wire sum, carryout;
 
-    behavioralFullAdder adder (sum, carryout, a, b, carryin);
+    structuralFullAdder adder1 (sum, carryout, a, b, carryin);
+    behavioralFullAdder adder2 (expectedsum, expectedcarryout, a, b, carryin);
 
     initial begin
-        // Your test code here
+    // Your test code here
+    $display("   Inputs   |  Expected:   |   Circuit:  ");
+    $display("a b carryin | sum carryout | sum carryout");
+    a=0;b=0;carryin=0; #1000
+    $display("%b %b %b       | %b   %b        | %b   %b   ", a, b, carryin, expectedsum, expectedcarryout, sum, carryout);
+    a=1;b=0;carryin=0; #1000
+    $display("%b %b %b       | %b   %b        | %b   %b   ", a, b, carryin, expectedsum, expectedcarryout, sum, carryout);
+    a=0;b=1;carryin=0; #1000
+    $display("%b %b %b       | %b   %b        | %b   %b   ", a, b, carryin, expectedsum, expectedcarryout, sum, carryout);
+    a=1;b=1;carryin=0; #1000
+    $display("%b %b %b       | %b   %b        | %b   %b   ", a, b, carryin, expectedsum, expectedcarryout, sum, carryout);
+
+
+    a=0;b=0;carryin=1; #1000
+    $display("%b %b %b       | %b   %b        | %b   %b   ", a, b, carryin, expectedsum, expectedcarryout, sum, carryout);
+    a=1;b=0;carryin=1; #1000
+    $display("%b %b %b       | %b   %b        | %b   %b   ", a, b, carryin, expectedsum, expectedcarryout, sum, carryout);
+    a=0;b=1;carryin=1; #1000
+    $display("%b %b %b       | %b   %b        | %b   %b   ", a, b, carryin, expectedsum, expectedcarryout, sum, carryout);
+    a=1;b=1;carryin=1; #1000
+    $display("%b %b %b       | %b   %b        | %b   %b   ", a, b, carryin, expectedsum, expectedcarryout, sum, carryout);
+
     end
 endmodule
--- multiplexer.v
+++ multiplexer.v
@@ -1,3 +1,8 @@
+// define gates with delays
+`define AND and #50
+`define OR or #50
+`define NOT not #50
+
 // Multiplexer circuit
 
 module behavioralMultiplexer
@@ -16,9 +21,35 @@ endmodule
 module structuralMultiplexer
 (
     output out,
-    input address0, address1,
+    input addr0, addr1,
     input in0, in1, in2, in3
 );
     // Your multiplexer code here
+not notaddr0(naddr0, addr0);
+not notaddr1(naddr1, addr1);
+
+and getin0(in0, naddr1, naddr0);
+and getin1(in1, naddr1, addr0);
+and getin2(in2, addr1, naddr0);
+and getin3(in3, addr1, addr0);
+
+// getting the nin0 nin1, nin2, and nin3 values just in case
+not getnin0(nin0, in0);
+not getnin1(nin1, in1);
+not getnin2(nin2, in2);
+not getnin3(nin3, in3);
+
+// getting values to pass into output orgate
+and geto1(and1, in0, in1);
+and geto2(and2, in2, in3);
+
+
+
+
+
+// Still unsure about how to get out value. and -> the 1s become Xs. or -> 0s
+// become Xs
+
+
 endmodule
 
