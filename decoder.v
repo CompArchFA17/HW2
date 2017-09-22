8eaa07ffd6e2a70b0a3921978215fa4b25400268
juicyslew <william.derksen@students.olin.edu>
2017-09-21 23:31:00 -0400

    The sweet release of finished homework

--- decoder.v
+++ decoder.v
@@ -1,5 +1,10 @@
 // Decoder circuit
-
+`define AND and #50
+`define OR or #50
+`define NOT not #50
+`define NAND nand #50
+`define NOR nor #50
+`define XOR xor #50
 module behavioralDecoder
 (
     output out0, out1, out2, out3,
@@ -14,9 +19,18 @@ endmodule
 module structuralDecoder
 (
     output out0, out1, out2, out3,
-    input address0, address1,
+    input in0, in1,
     input enable
 );
-    // Your decoder code here
+  wire in0, in1;
+  `NOT Nin0(nin0, in0);
+  `NOT Nin1(nin1, in1);
+  `AND preo0(preout0, nin0, nin1);
+  `AND preo1(preout1, nin1, in0);
+  `AND preo2(preout2, nin0, in1);
+  `AND preo3(preout3, in0, in1);
+  `AND o0(out0, preout0, enable);
+  `AND o1(out1, preout1, enable);
+  `AND o2(out2, preout2, enable);
+  `AND o3(out3, preout3, enable);
 endmodule
-
