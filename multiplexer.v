d174564a6194a79400028d3670b1842051993321
Tobias Shapinsky <tobiasshapinsky@gmail.com>
2017-09-21 22:24:48 -0400

    added images and dump lines to test suite

--- multiplexer.v
+++ multiplexer.v
@@ -1,5 +1,8 @@
 // Multiplexer circuit
-
+// define gates with delays
+`define AND and #50
+`define OR or #50
+`define NOT not #50
 module behavioralMultiplexer
 (
     output out,
@@ -19,6 +22,36 @@ module structuralMultiplexer
     input address0, address1,
     input in0, in1, in2, in3
 );
-    // Your multiplexer code here
+   wire   A0_;
+   wire   A1_;
+   wire   en0;
+   wire   en1;
+   wire   en2;
+   wire   en3;
+   wire   out0;
+   wire   out1;
+   wire   out2;
+   wire   out3;
+   wire   out01;
+   wire   out23;
+   
+   
+   
+
+   `NOT nA0(A0_, address0);
+   `NOT nA1(A1_, address1);
+   `AND E00(en0, A0_, A1_);
+   `AND E01(en1, address0, A1_);
+   `AND E02(en2, A0_, address1);
+   `AND E03(en3, address0, address1);
+   `AND O0(out0, in0, en0);
+   `AND O1(out1, in1, en1);
+   `AND O2(out2, in2, en2);
+   `AND O3(out3, in3, en3);
+   `OR O01(out01, out0, out1);
+   `OR O23(out23, out2, out3);
+   `OR O0123(out, out01, out23);
+   
+
 endmodule
 
