cc12680316fde9009d7d1dbed365a9f539b7e95b
Sung Park <sungwoo.park@students.olin.edu>
2017-09-23 23:46:59 -0400

    Add hw2

--- multiplexer.t.v
+++ multiplexer.t.v
@@ -4,4 +4,25 @@
 
 module testMultiplexer ();
   // Your test code here
+    reg addr0, addr1;
+    reg in0, in1, in2, in3;
+    wire out;
+
+    //behavioralMultiplexer multiplexer (out, addr0, addr1, in0, in1, in2, in3);
+    structuralMultiplexer multiplexer (out, addr0, addr1, in0, in1, in2, in3);
+     
+
+    initial begin
+    $dumpfile("multiplexer.vcd");
+    $dumpvars(0,testMultiplexer);
+    $display("A0 A1 I0 I1 I2 I3 | Output | Expected Output");
+    addr0=0;addr1=0;in0=0;in1=1;in2=0;in3=1; #1000
+    $display("%b %b %b %b %b %b | %b | 0", addr0, addr1, in0, in1, in2, in3, out);
+    addr0=1;addr1=0;in0=0;in1=1;in2=0;in3=1; #1000
+    $display("%b %b %b %b %b %b | %b | 1", addr0, addr1, in0, in1, in2, in3, out);
+    addr0=0;addr1=1;in0=0;in1=1;in2=0;in3=1; #1000
+    $display("%b %b %b %b %b %b | %b | 0", addr0, addr1, in0, in1, in2, in3, out);
+    addr0=1;addr1=1;in0=0;in1=1;in2=0;in3=1; #1000
+    $display("%b %b %b %b %b %b | %b | 1", addr0, addr1, in0, in1, in2, in3, out);
+    end
 endmodule
